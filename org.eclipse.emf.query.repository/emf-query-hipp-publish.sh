99fce409ef66c6694173b017421a7725d5cd834c
Anthony Hunter <anthonyh@ca.ibm.com>
2014-07-03 12:35:52 -0400

    Fix issues with the publish script

--- org.eclipse.emf.query.repository/emf-query-hipp-publish.sh
+++ org.eclipse.emf.query.repository/emf-query-hipp-publish.sh
@@ -50,7 +50,7 @@ fi
 # Determine the local update site we want to publish from
 localTarget=${WORKSPACE}/org.eclipse.emf.query.repository/target
 localUpdateSite=${localTarget}/repository/
-echo "Using local update-site: $localUpdateSite"
+echo "`date +%Y-%m-%d-%H:%M:%S` Using local update-site: $localUpdateSite"
 
 # Determine remote update site we want to promote to (integration and maintenance are published on interim site, stable builds on milestone site, release builds on releases site)
 case $buildType in
@@ -60,10 +60,10 @@ case $buildType in
 	r|R) remoteSite=releases ;;
 	*) exit 0 ;;
 esac
-echo "Publishing as $remoteSite ( $buildType ) build"
+echo "`date +%Y-%m-%d-%H:%M:%S` Publishing as $remoteSite ( $buildType ) build"
 remoteUpdateSiteBase="modeling/emf/query/updates/$remoteSite"
 remoteUpdateSite="/home/data/httpd/download.eclipse.org/$remoteUpdateSiteBase"
-echo "Publishing to remote update-site: $remoteUpdateSite"
+echo "`date +%Y-%m-%d-%H:%M:%S` Publishing to remote update-site: $remoteUpdateSite"
 
 if [ -z "$dropFilesLabel" -a "$buildType" != i ]; then
 	echo "Please provide a drop files label to append to the version (e.g. M5, RC1) if this is not an I build."
@@ -75,12 +75,12 @@ tmpDir="$localTarget/$JOB_NAME-publish-tmp"
 rm -fr $tmpDir
 mkdir -p $tmpDir
 cd $tmpDir
-echo "Working in `pwd`"
+echo "`date +%Y-%m-%d-%H:%M:%S` Working in `pwd`"
 
 # Download and prepare Eclipse SDK, which is needed to process the update site
-echo "Downloading eclipse to $PWD"
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.3.2-201402211700/eclipse-SDK-4.3.2-linux-gtk-x86_64.tar.gz .
-tar -xzf eclipse-SDK-4.3.2-linux-gtk-x86_64.tar.gz
+echo "`date +%Y-%m-%d-%H:%M:%S` Downloading eclipse to $PWD"
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.4-201406061215/eclipse-SDK-4.4-linux-gtk-x86_64.tar.gz .
+tar -xzf eclipse-SDK-4.4-linux-gtk-x86_64.tar.gz
 cd eclipse
 chmod 700 eclipse
 cd ..
@@ -89,46 +89,45 @@ if [ ! -d "eclipse" ]; then
 	exit
 fi
 # Prepare Eclipse SDK to provide WTP releng tools (used to postprocess repository, i.e set p2.mirrorsURL property)
-echo "Installing WTP Releng tools"
+echo "`date +%Y-%m-%d-%H:%M:%S` Installing WTP Releng tools"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/webtools/releng/repository/ -installIUs org.eclipse.wtp.releng.tools.feature.feature.group
 # Clean up
-echo "Cleaning up"
-rm eclipse-SDK-4.3.2-linux-gtk-x86_64.tar.gz
+rm eclipse-SDK-4.4-linux-gtk-x86_64.tar.gz
 
 # Generate drop files
-echo "Converting update site to runnable form"
+echo "`date +%Y-%m-%d-%H:%M:%S` Converting update site to runnable form"
 ./eclipse/eclipse -nosplash -consoleLog -application org.eclipse.equinox.p2.repository.repo2runnable -source file:$localUpdateSite -destination file:drops/eclipse
 qualifiedVersion=$(find $localUpdateSite/features/ -maxdepth 1 | grep "org.eclipse.emf.query_" | sed 's/.jar$//')
-echo "qualifiedVersion is $qualifiedVersion"
+echo "`date +%Y-%m-%d-%H:%M:%S` qualifiedVersion is $qualifiedVersion"
 qualifiedVersion=${qualifiedVersion#*_}
-echo "qualifiedVersion is $qualifiedVersion"
+echo "`date +%Y-%m-%d-%H:%M:%S` qualifiedVersion is $qualifiedVersion"
 qualifier=${qualifiedVersion##*.}
-echo "qualifier is $qualifier"
+echo "`date +%Y-%m-%d-%H:%M:%S` qualifier is $qualifier"
 qualifier=${qualifier#v}
-echo "qualifier is $qualifier"
+echo "`date +%Y-%m-%d-%H:%M:%S` qualifier is $qualifier"
 version=${qualifiedVersion%.*}
-echo "version is $version"
+echo "`date +%Y-%m-%d-%H:%M:%S` version is $version"
 dropDir="$(echo $buildType | tr '[:lower:]' '[:upper:]')$qualifier"
-echo "dropDir is $dropDir"
+echo "`date +%Y-%m-%d-%H:%M:%S` dropDir is $dropDir"
 localDropDir=drops/$version/$dropDir
-echo "Creating drop files in local directory $tmpDir/$localDropDir"
+echo "`date +%Y-%m-%d-%H:%M:%S` Creating drop files in local directory $tmpDir/$localDropDir"
 mkdir -p $localDropDir
 
 # Prepare local update site (merging is performed later, if required)
 stagedUpdateSite="updates/$remoteSite/$dropDir"
 mkdir -p $stagedUpdateSite
 cp -R $localUpdateSite/* $stagedUpdateSite
-echo "Copied $localUpdateSite to local directory $stagedUpdateSite."
+echo "`date +%Y-%m-%d-%H:%M:%S` Copied $localUpdateSite to local directory $stagedUpdateSite."
 
 # Append drop file suffix if one is specified				
 if [ -n "$dropFilesLabel" ]; then
 	version=$version$dropFilesLabel
-	echo "version is now $version"
+	echo "`date +%Y-%m-%d-%H:%M:%S` version is now $version"
 elif [ "$buildType" != r -a "$buildType" != R ]; then
 	version="$(echo $buildType | tr '[:lower:]' '[:upper:]')$qualifier"
-	echo "version is now $version"
+	echo "`date +%Y-%m-%d-%H:%M:%S` version is now $version"
 else
-	echo "version is now $version"
+	echo "`date +%Y-%m-%d-%H:%M:%S` version is now $version"
 fi
 				
 cp eclipse/epl-v10.html drops/eclipse
@@ -150,7 +149,7 @@ zip -r ../$localDropDir/emf-query-SDK-$version.zip \
 	eclipse/plugins/org.eclipse.emf.query.ocl.source_* \
 	eclipse/plugins/org.eclipse.emf.query.source_*
 md5sum ../$localDropDir/emf-query-SDK-$version.zip > ../$localDropDir/emf-query-SDK-$version.zip.md5
-echo "Created emf-query-SDK-$version.zip"
+echo "`date +%Y-%m-%d-%H:%M:%S` Created emf-query-SDK-$version.zip"
 			
 # emf-query runtime
 zip -r ../$localDropDir/emf-query-runtime-$version.zip \
@@ -160,7 +159,7 @@ zip -r ../$localDropDir/emf-query-runtime-$version.zip \
 	eclipse/plugins/org.eclipse.emf.query_* \
 	eclipse/plugins/org.eclipse.emf.query.ocl_*
 md5sum ../$localDropDir/emf-query-runtime-$version.zip > ../$localDropDir/emf-query-runtime-$version.zip.md5
-echo "Created emf-query-runtime-$version.zip"
+echo "`date +%Y-%m-%d-%H:%M:%S` Created emf-query-runtime-$version.zip"
 			
 # emf-query examples
 zip -r ../$localDropDir/emf-query-examples-$version.zip \
@@ -174,7 +173,7 @@ zip -r ../$localDropDir/emf-query-examples-$version.zip \
 	eclipse/plugins/org.eclipse.emf.query.examples.statements_* \
 	eclipse/plugins/org.eclipse.emf.query.examples.statements.source_*
 md5sum ../$localDropDir/emf-query-examples-$version.zip > ../$localDropDir/emf-query-examples-$version.zip.md5
-echo "Created emf-query-examples-$version.zip"
+echo "`date +%Y-%m-%d-%H:%M:%S` Created emf-query-examples-$version.zip"
 			
 # emf-query automated-tests
 zip -r ../$localDropDir/emf-query-automated-tests-$version.zip \
@@ -183,14 +182,14 @@ zip -r ../$localDropDir/emf-query-automated-tests-$version.zip \
 	eclipse/plugins/org.eclipse.emf.query.tests_* \
 	eclipse/plugins/org.eclipse.emf.query.ocl.tests_*
 md5sum ../$localDropDir/emf-query-automated-tests-$version.zip > ../$localDropDir/emf-query-automated-tests-$version.zip.md5
-echo "Created emf-query-automated-tests-$version.zip"
+echo "`date +%Y-%m-%d-%H:%M:%S` Created emf-query-automated-tests-$version.zip"
 	
 cd $tmpDir
 		
 cd $stagedUpdateSite
 zip -r ../../../$localDropDir/emf-query-Update-$version.zip features plugins binary artifacts.jar content.jar
 md5sum ../../../$localDropDir/emf-query-Update-$version.zip > ../../../$localDropDir/emf-query-Update-$version.zip.md5
-echo "Created emf-query-Update-Site-$version.zip"
+echo "`date +%Y-%m-%d-%H:%M:%S` Created emf-query-Update-Site-$version.zip"
 
 cd $tmpDir
 
@@ -199,22 +198,18 @@ echo "hudson.job.name=${JOB_NAME}" > $localDropDir/build.cfg
 echo "hudson.job.id=${BUILD_NUMBER} (${jobDir##*/})" >> $localDropDir/build.cfg
 echo "hudson.job.url=${BUILD_URL}" >> $localDropDir/build.cfg
 
-remoteDropDir=/home/data/httpd/download.eclipse.org/modeling/emf/query/downloads/drops/$dropDir
+remoteDropDir=/home/data/httpd/download.eclipse.org/modeling/emf/query/downloads/$localDropDir
 mkdir -p $remoteDropDir
 cp -R $localDropDir/* $remoteDropDir/
-
-#echo "Merging existing site into local one."
-#./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.metadata.repository.mirrorApplication -source file:$remoteUpdateSite -destination file:$stagedUpdateSite
-#./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication -source file:$remoteUpdateSite -destination file:$stagedUpdateSite
-#echo "Merged $remoteUpdateSite into local directory $stagedUpdateSite."
+echo "`date +%Y-%m-%d-%H:%M:%S` Published drop files in local directory $tmpDir/$localDropDir to $remoteDropDir"
 
 # Ensure p2.mirrorURLs property is used in update site
-echo "Setting p2.mirrorsURL to http://www.eclipse.org/downloads/download.php?format=xml&file=/$remoteUpdateSiteBase"
+echo "`date +%Y-%m-%d-%H:%M:%S` Setting p2.mirrorsURL to http://www.eclipse.org/downloads/download.php?format=xml&file=/$remoteUpdateSiteBase"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.wtp.releng.tools.addRepoProperties -vmargs -DartifactRepoDirectory=$PWD/$stagedUpdateSite -Dp2MirrorsURL="http://www.eclipse.org/downloads/download.php?format=xml&file=/$remoteUpdateSiteBase"
 
 # Create p2.index file
 if [ ! -e "$stagedUpdateSite/p2.index" ]; then
-	echo "Creating p2.index file"
+	echo "`date +%Y-%m-%d-%H:%M:%S` Creating p2.index file"
 	echo "version = 1" > $stagedUpdateSite/p2.index
 	echo "metadata.repository.factory.order = content.xml,\!" >> $stagedUpdateSite/p2.index
 	echo "artifact.repository.factory.order = artifacts.xml,\!" >> $stagedUpdateSite/p2.index
@@ -222,7 +217,7 @@ fi
 
 # Backup then clean remote update site
 if [ -d "$remoteUpdateSite" ]; then
-	echo "Creating backup of remote update site $remoteUpdateSite to $tmpDir/BACKUP."
+	echo "`date +%Y-%m-%d-%H:%M:%S` Creating backup of remote update site $remoteUpdateSite to $tmpDir/BACKUP."
 	if [ -d $tmpDir/BACKUP ]; then
 		rm -fr $tmpDir/BACKUP
 	fi
@@ -231,7 +226,7 @@ if [ -d "$remoteUpdateSite" ]; then
 	rm -fr $remoteUpdateSite
 fi
 
-echo "Publishing local $stagedUpdateSite directory to remote update site $remoteUpdateSite/$dropDir"
+echo "`date +%Y-%m-%d-%H:%M:%S` Publishing local $stagedUpdateSite directory to remote update site $remoteUpdateSite/$dropDir"
 mkdir -p $remoteUpdateSite
 cp -R $stagedUpdateSite $remoteUpdateSite
 
@@ -250,10 +245,10 @@ cat > p2.composite.repository.xml <<EOF
 </project>
 EOF
 
-echo "Update the composite update site"
+echo "`date +%Y-%m-%d-%H:%M:%S` Update the composite update site"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.ant.core.antRunner -buildfile p2.composite.repository.xml default
 
 # Clean up
-echo "Cleaning up"
+echo "`date +%Y-%m-%d-%H:%M:%S` Cleaning up"
 #rm -fr eclipse
 #rm -fr update-site
