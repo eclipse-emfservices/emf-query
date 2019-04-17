7b8f45666a0963fc5d34456ba91a93704ccad940
Pierre-Charles David <pierre-charles.david@obeo.fr>
2019-04-17 08:09:34 +0200

    [releng] Fix the path to the repo's JARs to remove
    Change-Id: I660068ad305e221f816274d8184b08409eed9648
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.emf.query.repository/emf-query-hipp-publish.sh
+++ org.eclipse.emf.query.repository/emf-query-hipp-publish.sh
@@ -243,7 +243,7 @@ mkdir -p $remoteUpdateSite
 cp -R $stagedUpdateSite $remoteUpdateSite
 
 echo "`date +%Y-%m-%d-%H:%M:%S` Refresh the composite update site"
-rm -f compositeArtifacts.jar compositeContents.jar
+rm -f ${remoteUpdateSite}/compositeArtifacts.jar ${remoteUpdateSite}/compositeContents.jar
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.ant.core.antRunner -buildfile p2.composite.repository.xml default
 
 # Clean up
